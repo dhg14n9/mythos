e98625d9108797f9225d8d89b61e92e471ed2979
Do Hoang Giang <dohoanggiang20@gmail.com>
2026-07-23 18:47:39 +0700

    tempo

--- src/eval/eval.rs
+++ src/eval/eval.rs
@@ -1,7 +1,7 @@
 use crate::board::board::Board;
 use crate::eval::S;
 use crate::eval::piece_square::psqt;
-use crate::types::Score;
+use crate::types::{Color, Score};
 
 fn taper(score: S, phase: i32) -> i32 {
     let mg_phase = phase.min(Board::GAME_PHASE_MAX);
@@ -10,6 +10,13 @@ fn taper(score: S, phase: i32) -> i32 {
 }
 
 pub fn eval(board: &Board) -> i32 {
-    let score = psqt(board);
+    let score = psqt(board) + tempo(board.stm());
     Score::score_color(taper(score, board.phase()), board.stm())
+}
+
+fn tempo(stm: Color) -> S {
+    S(20, 10) * match stm {
+        Color::White => 1,
+        Color::Black => -1
+    }
 }
