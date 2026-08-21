f397fde7c2f1f393ecd5f675e7cb4196ca24657a
Do Hoang Giang <dohoanggiang20@gmail.com>
2026-08-21 16:22:45 +0700

    refresh
    bench: 4157680

--- src/nnue/accumulator.rs
+++ src/nnue/accumulator.rs
@@ -1,8 +1,36 @@
+use std::ops::{Add, AddAssign};
 use crate::nnue::HL;
 use crate::types::{Color, Piece, Square};
 
 #[repr(C, align(64))]
+#[derive(Copy, Clone)]
 pub struct Accumulator([i16; HL]);
+
+impl Accumulator {
+    pub fn empty() -> Self {
+        Self([0; HL])
+    }
+}
+
+impl Add for Accumulator {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut output = Self::empty();
+        for i in 0..HL {
+            output.0[i] = self.0[i] + rhs.0[i]
+        }
+        output
+    }
+}
+
+impl AddAssign for Accumulator {
+    fn add_assign(&mut self, rhs: Self) {
+        for i in 0..HL {
+            self.0[i] += rhs.0[i]
+        }
+    }
+}
 
 pub fn feature_index(perspective: Color, piece: Piece, square: Square) -> usize {
     (piece.piece_type() as usize) * 64 + (square.relative_to(perspective) as usize) + if perspective == piece.color() { 0 } else { 384 }
--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -1,5 +1,7 @@
-use crate::nnue::accumulator::Accumulator;
+use crate::board::board::Board;
+use crate::nnue::accumulator::{feature_index, Accumulator};
 use crate::nnue::{HL, INPUT};
+use crate::types::Color;
 
 const _: () = assert!(size_of::<Network>() == 394_816);
 
@@ -11,4 +13,15 @@ pub struct Network {
     output_bias: i16
 }
 
+pub fn refresh(net: &Network, board: &Board, perspective: Color) -> Accumulator {
+    let mut result = net.feature_bias;
+    let occ = board.occ();
 
+    for square in occ {
+        let piece = board.piece_at(square);
+        let index = feature_index(perspective, piece, square);
+        result += net.feature_weights[index]
+    }
+
+    result
+}
